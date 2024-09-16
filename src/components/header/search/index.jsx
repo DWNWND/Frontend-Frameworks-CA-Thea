4102440f43d3957bb4e6f0995a5a8f61f2b36196
Thea <theola51924@stud.noroff.no>
2024-09-16 09:33:07 +0700

    made a searchbar component using styled components

---
+++ src/components/header/search/index.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import SearchIcon from "../../../images/search.svg";
+
+const SearchForm = styled.form`
+  display: flex;
+  width: 100%;
+`;
+
+const SearchButton = styled.button`
+  background-color: ${(props) => props.theme.colors.primary};
+  border: none;
+  padding: 0.3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SearchInput = styled.input`
+  border: none;
+  border-radius: 0;
+  background-color: ${(props) => props.theme.colors.searchbarBackground};
+  color: ${(props) => props.theme.colors.textDarkGray};
+  padding-left: 0.8rem;
+  width: 100%;
+  font-size: 1rem;
+`;
+
+export default function Searchbar() {
+  return (
+    <SearchForm id="searchForm">
+      <SearchInput id="searchInput" type="search" placeholder="Search..." aria-label="Search" />
+      <SearchButton id="searchButton" type="submit" aria-label="Search">
+        <img src={SearchIcon} alt="Search icon" />
+      </SearchButton>
+    </SearchForm>
+  );
+}
