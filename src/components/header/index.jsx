4fcedd7387e35b3f0e10c389d3518e82fc0dbc6d
Thea <theola51924@stud.noroff.no>
2024-09-16 09:37:03 +0700

    added a header component containing the logo, a back-arrow icon(for later use), the searchbar component, the cart component and the hamburger/nav component. Using styled components on the wrapper of the header and to wrapp the icons/other components

---
+++ src/components/header/index.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import LogoIcon from "../../images/logo.png";
+import BackArrowIcon from "../../images/back.svg";
+import Searchbar from "./search";
+import HamburgerMenu from "./nav";
+import Cart from "./cart";
+
+const Wrapper = styled.header`
+  display: flex;
+  gap: 1.5rem;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1200px;
+  padding: 1rem;
+  background-color: var(--color-white);
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 24px;
+`;
+
+export default function Header() {
+  return (
+    <Wrapper>
+      <IconWrapper>
+        <img src={LogoIcon} alt="Lazz logo, click to go to home page"/>
+      </IconWrapper>
+      {/* <IconWrapper>
+        <img src={BackArrowIcon} alt="fill-in-later" />
+      </IconWrapper> */}
+      <Searchbar />
+      <IconWrapper>
+        <Cart />
+      </IconWrapper>
+      <HamburgerMenu />
+    </Wrapper>
+  );
+}
