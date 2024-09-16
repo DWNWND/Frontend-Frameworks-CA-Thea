fae7c4f8592e59198ef44be243dfeda7c8784836
Thea <theola51924@stud.noroff.no>
2024-09-16 09:35:26 +0700

    added a hamburgermenu with a nav component using styled components and useState

---
+++ src/components/header/nav/index.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import styled from "styled-components";
+// import HamburgerMenuIcon from "../../../images/menu.svg";
+// import CrossIcon from "../../../images/cross.svg";
+
+const Nav = styled.nav`
+  background-color: white;
+  position: absolute;
+  top: 100px;
+  left: 0;
+  width: 100%;
+  z-index: 100;
+`;
+
+const HamburgerMenuBtn = styled.button`
+  background-color: white;
+  border: none;
+  padding: 0 0 0 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-height: 24px;
+`;
+
+function OpenMenu() {
+  return (
+    <Nav>
+      <ul className="main-nav-ul">
+        <li>
+          <a href="#" className="main-nav-a">Home</a>
+        </li>
+        <li>
+          <a href="#" className="main-nav-a">Categories</a>
+          <ul className="sub-nav-ul">
+            <li>
+              <a href="#">Electronics</a>
+            </li>
+            <li>
+              <a href="#">Audio</a>
+            </li>
+            <li>
+              <a href="#">Fashion</a>
+            </li>
+            <li>
+              <a href="#">Beauty</a>
+            </li>
+            <li>
+              <a href="#">Watches</a>
+            </li>
+            <li>
+              <a href="#">Shoes</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#" className="main-nav-a">Contact us</a>
+        </li>
+        <li>
+          <a href="#" className="main-nav-a">Help & support</a>
+        </li>
+      </ul>
+    </Nav>
+  );
+}
+
+export default function HamburgerMenu() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function onHamburgerMenuClick() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  return (
+    <>
+      <HamburgerMenuBtn onClick={() => onHamburgerMenuClick()}>
+        {/* <img onClick={() => onHamburgerMenuClick()} src={isMenuOpen ? CrossIcon : HamburgerMenuIcon} alt="Menu icon" className="menu-transition" /> */}
+        <div className={isMenuOpen ? "burger burger-squeeze open" : "burger burger-squeeze"}>
+          <div className="burger-lines"></div>
+        </div>
+      </HamburgerMenuBtn>
+      {isMenuOpen ? <OpenMenu /> : <></>}
+    </>
+  );
+}
