f237624116143cbf2ce19d1046ee3699cafc0b38
Thea <theola51924@stud.noroff.no>
2024-09-16 09:33:36 +0700

    made a cart icon component

---
+++ src/components/header/cart/index.jsx
@@ -0,0 +1,9 @@
+import CartIcon from "../../../images/cart.svg";
+
+export default function Cart() {
+  return (
+    <>
+      <img src={CartIcon} alt="Shopping cart icon, click to go to shopping cart" />
+    </>
+  );
+}
