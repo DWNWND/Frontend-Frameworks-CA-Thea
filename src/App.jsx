6da47fe511f05c5a3af2780fe03ba8423b1b0e42
Thea <theola51924@stud.noroff.no>
2024-09-17 11:55:34 +0700

    added a Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import { useState, useEffect } from "react";
-import Header from "./components/header/index.jsx";
-import Footer from "./components/footer/index.jsx";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function Product() {
+  return <div>Product</div>;
+}
+
+function Checkout() {
+  return <div>Checkout</div>;
+}
+
+function CheckoutSuccess() {
+  return <div>success</div>;
+}
+
+function Contact() {
+  return <div>Contact</div>;
+}
+
+function RouteNotFound() {
+  return <div>Page not found</div>;
+}
+
 
 function App() {
   return (
     <>
-      <Header />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="product/:id" element={<Product />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="success" element={<CheckoutSuccess />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<RouteNotFound />} />
+        </Route>
+      </Routes>
     </>
   );
 }
