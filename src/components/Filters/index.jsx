10c74bf3cbe8e4c4c7b97a5e3e7cf7591a1e0df1
Thea <theola51924@stud.noroff.no>
2024-09-17 21:07:06 +0700

    added a Filter component

---
+++ src/components/Filters/index.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import styles from "./Filters.module.css";
+
+export default function Filters({ page = "/" }) {
+  const [isActive, setActive] = useState("");
+
+  const toggleClass = (event) => {
+    setActive(event.target.id);
+  };
+
+  if (page === "/") {
+    return (
+      <div className={styles.wrapper}>
+        <button className={`${isActive == "TopSales" ? styles.current : ""} ${styles.filter}`} id="TopSales" onClick={toggleClass}>
+          Top sales
+        </button>
+        <button className={`${isActive == "DiscountedItems" ? styles.current : ""} ${styles.filter}`} id="DiscountedItems" onClick={toggleClass}>
+          Discounted items
+        </button>
+      </div>
+    );
+  }
+  if (page === "/products") {
+    return (
+      <div className={styles.wrapper}>
+        <button className={`${isActive == "BestMatch" ? styles.current : ""} ${styles.filter}`} id="BestMatch" onClick={toggleClass}>
+          Best match
+        </button>
+        <button className={`${isActive == "TopSales" ? styles.current : ""} ${styles.filter}`} id="TopSales" onClick={toggleClass}>
+          Top sales
+        </button>
+        <button className={`${isActive == "DiscountedItems" ? styles.current : ""} ${styles.filter}`} id="DiscountedItems" onClick={toggleClass}>
+          Discounted items
+        </button>
+      </div>
+    );
+  }
+}
