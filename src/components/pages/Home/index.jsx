25474c50cd65f27d66400febcfdac5567ae27654
Thea <theola51924@stud.noroff.no>
2024-09-17 21:08:09 +0700

    added BrandCommercial, Categories, Filters and ProductCards to the Home component

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,10 +1,15 @@
 import ProductCards from "../../ProductCards";
+import Categories from "./Categories";
+import BrandCommercial from "./BrandCommercial";
+import Filters from "../../Filters";
 import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.wrapper}>
-      <h1>Home</h1>
+      <BrandCommercial/>
+      <Categories/>
+      <Filters />
       <ProductCards />
     </div>
   );
