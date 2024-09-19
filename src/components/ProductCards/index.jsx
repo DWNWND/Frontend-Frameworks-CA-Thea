e468de855e7abdd104333214856b1c73589e3e4c
Thea <theola51924@stud.noroff.no>
2024-09-19 20:18:57 +0700

    restructured the rendering of products in ProductCard, Product and Filter components to work with createContext and useContext

--- src/components/ProductCards/index.jsx
+++ src/components/ProductCards/index.jsx
@@ -1,19 +1,26 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import styles from "./ProductCards.module.css";
+import { useEffect, useState, createContext, useContext } from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
 import Price from "../Price";
 import Ratings from "../Ratings";
+import styles from "./ProductCards.module.css";
+import Filters from "../Filters";
+import ProductCards from "./card.jsx";
+import { ProductTagContext } from "../pages/Product/index.jsx";
 
 const url = "https://v2.api.noroff.dev/online-shop";
 
-export default function ProductCards({ category }) {
-  const [products, setProducts] = useState([]);
-  const [filterButtons, setfilterButtons] = useState([]);
-  const [filterActive, setfilterActive] = useState(false);
-  const [selectedCategory, setSelectedCategory] = useState("");
+export const FilterContext = createContext();
+export const ProductsContext = createContext();
 
+export function ProductsToDisplay() {
+  const tag = useContext(ProductTagContext);
+  const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [filters, setFilters] = useState("topSales");
+
+  let { category } = useParams();
+  const location = useLocation();
 
   useEffect(() => {
     async function getData() {
@@ -25,64 +32,51 @@ export default function ProductCards({ category }) {
         const json = await response.json();
         const data = json.data;
 
-        const topSalesProducts = data.sort((a, b) => b.reviews.length - a.reviews.length);
-        setProducts(topSalesProducts);
+        if (filters === "topSales") {
+          const sortedProducts = data.sort((a, b) => b.reviews.length - a.reviews.length);
+          setProducts(sortedProducts);
+        }
+        if (filters === "discountedItems") {
+          const filteredProducts = data.filter((product) => product.discountedPrice !== product.price);
+          setProducts(filteredProducts);
+        }
+        if (location.pathname.includes(category)) {
+          const categorizedProducts = data.filter((product) => product.tags.includes(category));
+          setProducts(categorizedProducts);
+
+          if (filters === "topSales") {
+            const sortedProducts = categorizedProducts.sort((a, b) => b.reviews.length - a.reviews.length);
+            setProducts(sortedProducts);
+          }
+          if (filters === "discountedItems") {
+            const filteredProducts = categorizedProducts.filter((product) => product.discountedPrice !== product.price);
+            setProducts(filteredProducts);
+          }
+        }
+        if (location.pathname.includes("/product/")) {
+          const taggedProducts = data.filter((product) => product.tags.includes(tag));
+          setProducts(taggedProducts);
+
+
+          if (filters === "topSales") {
+            const sortedProducts = taggedProducts.sort((a, b) => b.reviews.length - a.reviews.length);
+            setProducts(sortedProducts);
+          }
+          if (filters === "discountedItems") {
+            const filteredProducts = taggedProducts.filter((product) => product.discountedPrice !== product.price);
+            setProducts(filteredProducts);
+          }
+        }
 
         setIsLoading(false);
       } catch (error) {
         setIsLoading(false);
         setIsError(true);
+        console.log(error);
       }
     }
     getData();
-  }, []);
-
-  // useEffect(() => {
-  //   const filtersContainer = document.getElementById("filters");
-  //   const filtersAll = filtersContainer.querySelectorAll("button");
-  //   setfilterButtons(filtersAll);
-  // }, [filterActive]);
-
-  // filterButtons.forEach((button) => {
-  //   button.addEventListener("click", () => {
-  //     if (button.id === "TopSales") {
-  //       const sortedProducts = products.sort((a, b) => b.reviews.length - a.reviews.length);
-  //       setProducts(sortedProducts);
-  //       setfilterActive(false);
-  //       setfilterButtons([]);
-  //     }
-
-  //     if (button.id === "DiscountedItems") {
-  //       const filteredProducts = products.filter((product) => product.discountedPrice !== product.price);
-  //       setProducts(filteredProducts);
-  //       setfilterActive(false);
-  //       setfilterButtons([]);
-  //     }
-  //   });
-  // });
-
-  const courses = category;
-
-  const r = products.filter((d) => d.tags.every((c) => category.includes(c.tags)));
-  console.log(r);
-
-  // let categorized = [];
-
-  // function checkForCategory() {
-  //   for (let i = 0; i < products.length; i++) {
-  //     products[i].tags.forEach((tags) => {
-  //       if (tags.includes(category)) {
-  //         categorized.push(products[i]);
-  //       }
-  //     });
-  //   }
-  // }
-
-  // if (category) {
-  //   checkForCategory();
-  //   console.log(categorized);
-  //   // setProducts(categorized);
-  // }
+  }, [filters]);
 
   if (isLoading) {
     return <div>Loading posts</div>;
@@ -91,26 +85,19 @@ export default function ProductCards({ category }) {
   if (isError) {
     return <div>Error loading data</div>;
   }
+
   return (
     <>
-      <Product products={products}></Product>
+      <FilterContext.Provider value={{ filters, setFilters }}>
+        <div>
+          <Filters page={location} />
+        </div>
+      </FilterContext.Provider>
+      <ProductsContext.Provider value={{ products, setProducts }}>
+        <div>
+          <ProductCards />
+        </div>
+      </ProductsContext.Provider>
     </>
   );
-}
-
-function Product({ products }) {
-  <div className={styles.wrapper}>
-    {products.map((product) => (
-      <Link to={"/product/" + product.id} key={product.id} className={styles.card}>
-        <div className={styles.imageContainer}>
-          <img src={product.image.url} alt={product.image.alt} />
-        </div>
-        <div className={styles.infoContainer}>
-          <h2>{product.title}</h2>
-          <Price originalPrice={product.price} discountedPrice={product.discountedPrice} page="" />
-          <Ratings rating={product.rating} reviews={product.reviews} />
-        </div>
-      </Link>
-    ))}
-  </div>;
 }
--- src/components/pages/ProductsList/index.jsx
+++ src/components/pages/ProductsList/index.jsx
@@ -1,7 +1,10 @@
+import { ProductsToDisplay } from "../../ProductCards";
+import styles from "./ProductsList.module.css";
+
 export default function ProductsList() {
   return (
-    <div>
-      <h1>ProductsList</h1>
+    <div className={styles.wrapper}>
+      <ProductsToDisplay />
     </div>
   );
 }
