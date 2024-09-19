c667dedb00efce7603d02389b310f7ae4b26dac3
Thea <theola51924@stud.noroff.no>
2024-09-19 08:46:42 +0700

    trying to figure out how to filter the data form the api before displaying - moving to next branch

--- src/components/ProductCards/index.jsx
+++ src/components/ProductCards/index.jsx
@@ -6,10 +6,11 @@ import Ratings from "../Ratings";
 
 const url = "https://v2.api.noroff.dev/online-shop";
 
-export default function ProductCards() {
+export default function ProductCards({ category }) {
   const [products, setProducts] = useState([]);
   const [filterButtons, setfilterButtons] = useState([]);
   const [filterActive, setfilterActive] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
@@ -36,11 +37,52 @@ export default function ProductCards() {
     getData();
   }, []);
 
-  useEffect(() => {
-    const filtersContainer = document.getElementById("filters");
-    const filtersAll = filtersContainer.querySelectorAll("button");
-    setfilterButtons(filtersAll);
-  }, [filterActive]);
+  // useEffect(() => {
+  //   const filtersContainer = document.getElementById("filters");
+  //   const filtersAll = filtersContainer.querySelectorAll("button");
+  //   setfilterButtons(filtersAll);
+  // }, [filterActive]);
+
+  // filterButtons.forEach((button) => {
+  //   button.addEventListener("click", () => {
+  //     if (button.id === "TopSales") {
+  //       const sortedProducts = products.sort((a, b) => b.reviews.length - a.reviews.length);
+  //       setProducts(sortedProducts);
+  //       setfilterActive(false);
+  //       setfilterButtons([]);
+  //     }
+
+  //     if (button.id === "DiscountedItems") {
+  //       const filteredProducts = products.filter((product) => product.discountedPrice !== product.price);
+  //       setProducts(filteredProducts);
+  //       setfilterActive(false);
+  //       setfilterButtons([]);
+  //     }
+  //   });
+  // });
+
+  const courses = category;
+
+  const r = products.filter((d) => d.tags.every((c) => category.includes(c.tags)));
+  console.log(r);
+
+  // let categorized = [];
+
+  // function checkForCategory() {
+  //   for (let i = 0; i < products.length; i++) {
+  //     products[i].tags.forEach((tags) => {
+  //       if (tags.includes(category)) {
+  //         categorized.push(products[i]);
+  //       }
+  //     });
+  //   }
+  // }
+
+  // if (category) {
+  //   checkForCategory();
+  //   console.log(categorized);
+  //   // setProducts(categorized);
+  // }
 
   if (isLoading) {
     return <div>Loading posts</div>;
@@ -49,39 +91,26 @@ export default function ProductCards() {
   if (isError) {
     return <div>Error loading data</div>;
   }
-
-  filterButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      if (button.id === "TopSales") {
-        const sortedProducts = products.sort((a, b) => b.reviews.length - a.reviews.length);
-        setProducts(sortedProducts);
-        setfilterActive(false);
-        setfilterButtons([]);
-      }
-
-      if (button.id === "DiscountedItems") {
-        const filteredProducts = products.filter((product) => product.discountedPrice !== product.price);
-        setProducts(filteredProducts);
-        setfilterActive(false);
-        setfilterButtons([]);
-      }
-    });
-  });
-
   return (
-    <div className={styles.wrapper}>
-      {products.map((product) => (
-        <Link to={"/product/" + product.id} key={product.id} className={styles.card}>
-          <div className={styles.imageContainer}>
-            <img src={product.image.url} alt={product.image.alt} />
-          </div>
-          <div className={styles.infoContainer}>
-            <h2>{product.title}</h2>
-            <Price originalPrice={product.price} discountedPrice={product.discountedPrice} page="" />
-            <Ratings rating={product.rating} reviews={product.reviews} />
-          </div>
-        </Link>
-      ))}
-    </div>
+    <>
+      <Product products={products}></Product>
+    </>
   );
+}
+
+function Product({ products }) {
+  <div className={styles.wrapper}>
+    {products.map((product) => (
+      <Link to={"/product/" + product.id} key={product.id} className={styles.card}>
+        <div className={styles.imageContainer}>
+          <img src={product.image.url} alt={product.image.alt} />
+        </div>
+        <div className={styles.infoContainer}>
+          <h2>{product.title}</h2>
+          <Price originalPrice={product.price} discountedPrice={product.discountedPrice} page="" />
+          <Ratings rating={product.rating} reviews={product.reviews} />
+        </div>
+      </Link>
+    ))}
+  </div>;
 }
