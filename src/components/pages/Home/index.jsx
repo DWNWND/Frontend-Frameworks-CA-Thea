73a07930e3fd0757d8c20b69b54c5d374c49f126
Thea <theola51924@stud.noroff.no>
2024-09-19 20:19:44 +0700

    moved the Filter component inside the ProductsToDisplay component

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import ProductCards from "../../ProductCards";
+import { ProductsToDisplay } from "../../ProductCards";
 import Categories from "./Categories";
 import BrandCommercial from "./BrandCommercial";
 import Filters from "../../Filters";
@@ -7,10 +7,9 @@ import styles from "./Home.module.css";
 export default function Home() {
   return (
     <div className={styles.wrapper}>
-      <BrandCommercial/>
-      <Categories/>
-      <Filters />
-      <ProductCards />
+      <BrandCommercial />
+      <Categories />
+      <ProductsToDisplay />
     </div>
   );
 }
