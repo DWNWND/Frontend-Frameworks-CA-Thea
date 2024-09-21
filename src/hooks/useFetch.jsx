8060edafc39c2810d8ae42a351c3895cb55a79e2
Thea <theola51924@stud.noroff.no>
2024-09-21 16:31:00 +0700

    build a custom hook to call the API

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+// API fetch hook
+export default function useApi(url) {
+  const [data, setData] = useState([]);
+  const [tag, setTag] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+        const fetchedData = await fetch(url);
+        const json = await fetchedData.json();
+        setData(json);
+        setTag(json.data.tags[0]);
+      } catch (error) {
+        console.log(error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getData();
+  }, [url]);
+  return { data, tag, isLoading, isError };
+}
