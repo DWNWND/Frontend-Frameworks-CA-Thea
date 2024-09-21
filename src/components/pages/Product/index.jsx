bb1e7a5da87af0b51f33ad2f9b2746ef4ae02207
Thea <theola51924@stud.noroff.no>
2024-09-21 16:34:39 +0700

    removed the old API call from the Product component to use the new Hook instead

--- src/components/pages/Product/index.jsx
+++ src/components/pages/Product/index.jsx
@@ -1,52 +1,24 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState, createContext } from "react";
-import { Link } from "react-router-dom";
+import { useState, createContext } from "react";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./Product.module.css";
 import Price from "../../Price";
 import Ratings from "../../Ratings";
 import { ProductsToDisplay } from "../../ProductCards";
+import Footer from "../../Layout/Footer";
+import useFetch from "../../../hooks/useFetch";
 
 const url = "https://v2.api.noroff.dev/online-shop";
 
 export const ProductTagContext = createContext();
+export const ProductObjectContext = createContext();
 
 export function Product() {
-  const [data, setData] = useState(null);
-  const [tag, setTag] = useState(null);
+  let { id } = useParams();
+  const { data, tag, isLoading, isError } = useFetch(url + "/" + id);
   const [descriptionOpen, setDescriptionOpen] = useState(false);
   const [reviewsOpen, setReviewsOpen] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  let { id } = useParams();
-
-  function toggleDescription() {
-    setDescriptionOpen(!descriptionOpen);
-  }
-
-  function toggleReviews() {
-    setReviewsOpen(!reviewsOpen);
-  }
-
-  useEffect(() => {
-    async function getData(url) {
-      try {
-        setIsLoading(true);
-        setIsError(false);
-
-        const response = await fetch(url);
-        const json = await response.json();
-
-        setData(json);
-
-        setTag(json.data.tags[0]);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    getData(`${url}/${id}`);
-  }, [id]);
+  const location = useLocation();
 
   if (isLoading || !data) {
     return <div>Loading</div>;
@@ -57,77 +29,93 @@ export function Product() {
   }
 
   const product = data.data;
-  const page = product.category;
+
+  function toggleDescription() {
+    setDescriptionOpen(!descriptionOpen);
+  }
+
+  function toggleReviews() {
+    setReviewsOpen(!reviewsOpen);
+  }
 
   return (
-    <ProductTagContext.Provider value={tag}>
-      <div className={styles.wrapper}>
-        <div className={styles.breadcrumbs}>
-          <Link to="/" className={styles.crumb}>
-            home
-          </Link>{" "}
-          /
-          <Link to={`/products/${tag}`} className={styles.crumb}>
-            {tag}
-          </Link>{" "}
-          / <div className={styles.active}>{product.title}</div>
-        </div>
-        <div className={styles.imageWrapper}>
-          <img src={product.image.url} alt={product.image.alt}></img>
-        </div>
-        <div className={styles.productInfoContainer}>
-          <div className={styles.infoWrapper}>
-            <h1>{product.title}</h1>
-            <div className={styles.likeShareWrapper}>
-              <div>Like</div>
-              <div>Share</div>
-            </div>
-          </div>
-          <div className={styles.infoWrapper}>
-            <Price originalPrice={product.price} discountedPrice={product.discountedPrice} page="/product/"></Price>
-            <Ratings rating={product.rating} reviews={product.reviews} section=""></Ratings>
-          </div>
-        </div>
-        <div className={styles.extendedInfoContainer}>
-          <div className={styles.extendedInfoWrapper}>
-            <div className={styles.infoHeader} onClick={() => toggleDescription()}>
-              <h2>Description</h2>
-              <div className={styles.arrowContainer}>
-                <div className={`${styles.arrow} ${descriptionOpen ? styles.down : styles.up}`}></div>
+    <>
+      {product ? (
+        <>
+          <ProductTagContext.Provider value={tag}>
+            <div className={styles.wrapper}>
+              <div className={styles.breadcrumbs}>
+                <Link to="/" className={styles.crumb}>
+                  home
+                </Link>{" "}
+                /
+                <Link to={`/products/${tag}`} className={styles.crumb}>
+                  {tag}
+                </Link>{" "}
+                / <div className={styles.active}>{product.title}</div>
               </div>
-            </div>
-            <p className={`${styles.content} ${descriptionOpen ? styles.open : styles.hidden}`}>{product.description}</p>
-          </div>
-
-          <div className={styles.extendedInfoWrapper}>
-            <div className={styles.infoHeader} onClick={() => toggleReviews()}>
-              <h2>Reviews ({product.reviews.length})</h2>
-              <div className={styles.arrowContainer}>
-                <div className={`${styles.arrow} ${reviewsOpen ? styles.down : styles.up}`}></div>
+              <div className={styles.imageWrapper}>
+                <img src={product.image.url} alt={product.image.alt}></img>
               </div>
-            </div>
-            <div className={`${styles.reviewsContainer} ${styles.content} ${reviewsOpen ? styles.open : styles.hidden}`}>
-              {product.reviews.map((review) => {
-                return (
-                  <div key={review.id} className={styles.reviewWrapper}>
-                    <div className={styles.reviewHeader}>
-                      <div className={styles.reviewUser}>{review.username}</div>
-                      <Ratings rating={review.rating} reviews={1} section="reviews"></Ratings>
-                    </div>
-                    <p>{review.description}</p>
+              <div className={styles.productInfoContainer}>
+                <div className={styles.infoWrapper}>
+                  <h1>{product.title}</h1>
+                  <div className={styles.likeShareWrapper}>
+                    <div>Like</div>
+                    <div>Share</div>
                   </div>
-                );
-              })}
+                </div>
+                <div className={styles.infoWrapper}>
+                  <Price originalPrice={product.price} discountedPrice={product.discountedPrice} page="/product/"></Price>
+                  <Ratings rating={product.rating} reviews={product.reviews} section=""></Ratings>
+                </div>
+              </div>
+              <div className={styles.extendedInfoContainer}>
+                <div className={styles.extendedInfoWrapper}>
+                  <div className={styles.infoHeader} onClick={() => toggleDescription()}>
+                    <h2>Description</h2>
+                    <div className={styles.arrowContainer}>
+                      <div className={`${styles.arrow} ${descriptionOpen ? styles.down : styles.up}`}></div>
+                    </div>
+                  </div>
+                  <p className={`${styles.content} ${descriptionOpen ? styles.open : styles.hidden}`}>{product.description}</p>
+                </div>
+
+                <div className={styles.extendedInfoWrapper}>
+                  <div className={styles.infoHeader} onClick={() => toggleReviews()}>
+                    <h2>Reviews ({product.reviews.length})</h2>
+                    <div className={styles.arrowContainer}>
+                      <div className={`${styles.arrow} ${reviewsOpen ? styles.down : styles.up}`}></div>
+                    </div>
+                  </div>
+                  <div className={`${styles.reviewsContainer} ${styles.content} ${reviewsOpen ? styles.open : styles.hidden}`}>
+                    {product.reviews.map((review) => {
+                      return (
+                        <div key={review.id} className={styles.reviewWrapper}>
+                          <div className={styles.reviewHeader}>
+                            <div className={styles.reviewUser}>{review.username}</div>
+                            <Ratings rating={review.rating} reviews={1} section="reviews"></Ratings>
+                          </div>
+                          <p>{review.description}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div>
+                  <div className={styles.infoHeader}>
+                    <h2>Similar products</h2>
+                  </div>
+                  <ProductsToDisplay />
+                </div>
+              </div>
             </div>
-          </div>
-          <div>
-            <div className={styles.infoHeader}>
-              <h2>Similar products</h2>
-            </div>
-            <ProductsToDisplay />
-          </div>
-        </div>
-      </div>
-    </ProductTagContext.Provider>
+          </ProductTagContext.Provider>
+          <ProductObjectContext.Provider value={product}>
+            <Footer page={location.pathname} />
+          </ProductObjectContext.Provider>
+        </>
+      ) : null}
+    </>
   );
 }
