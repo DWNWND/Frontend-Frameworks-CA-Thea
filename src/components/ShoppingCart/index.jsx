ee4d337a23164b6b881c20eeb9e20db17fd92a63
Thea <theola51924@stud.noroff.no>
2024-09-23 14:14:59 +0700

    rewritten the ShoppingCart component to use cart State through OutletContext and LS

--- src/components/ShoppingCart/index.jsx
+++ src/components/ShoppingCart/index.jsx
@@ -1,12 +1,12 @@
 import styles from "./ShoppingCart.module.css";
 import { Link, useLocation } from "react-router-dom";
-import { useEffect, useReducer, useState } from "react";
-// import ProductCards from "../ProductCards/card";
+import { useEffect, useReducer, useState, createContext, useContext } from "react";
 import useFetch from "../../hooks/useFetch";
 import Price from "../Price";
 import Ratings from "../Ratings";
 import Footer from "../Layout/Footer";
-import Quantity from "../Quanity";
+import { Quantity } from "../quanity";
+import { useOutletContext } from "react-router-dom";
 
 export const initialState = { cart: [], total: 0 };
 
@@ -77,56 +77,54 @@ export function reducer(state, action) {
   }
 }
 
+
 export function ShoppingCart() {
-  // const [cart, setCart] = useState([]);
-
+  const { cart, setCart } = useOutletContext();
   const [state, dispatch] = useReducer(reducer, initialState);
-
   const location = useLocation();
 
-  const itemsInLocalStorage = localStorage.getItem("shopping-cart");
-  const productsInCart = JSON.parse(itemsInLocalStorage);
+  if (!cart) {
+    return (
+      <>
+        <div>Loading</div>
+      </>
+    );
+  } else {
+    let newProductsArray = [];
 
-  return (
-    <>
-      {productsInCart ? (
-        <>
-          <div className={styles.wrapper}>
-            {productsInCart.map((product) => (
-              <ProductCartCards product={product} key={product.id}></ProductCartCards>
-            ))}
-          </div>
-          <Footer page={location.pathname} />
-        </>
-      ) : null}
+    function checkProductQuantity() {
+      const shoppingCart = JSON.parse(localStorage.getItem("shopping-cart"));
 
-      {/* <div>
-        {productsInCart.map((product) => (
-          <div key={product.id}>
-            <button onClick={() => dispatch({ type: "addProduct", payload: product })}>Add {product.title}</button>
-            <button onClick={() => dispatch({ type: "removeProduct", payload: product })}>Remove {product.title}</button>
-          </div>
-        ))}
-        <div>
-          <hr />
-          <button onClick={() => dispatch({ type: "clearCart" })}>Clear cart</button>
-          <button onClick={() => dispatch({ type: "getTotal" })}>Get total</button>
-        </div>
-        <div>{state.total}</div>
-        <hr />
-        <div>
-          {state.cart.map((product) => (
-            <div key={product.id}>
-              {product.quantity} - {product.title} - {product.discountedPrice}
+      if (shoppingCart) {
+        newProductsArray = shoppingCart.filter((product) => product.quantity > 0);
+        localStorage.setItem("shopping-cart", JSON.stringify(newProductsArray));
+      } else {
+        throw new Error("Product you want to decrement is not found in the cart");
+      }
+    }
+
+    useEffect(() => {
+      checkProductQuantity();
+    }, [cart, location.pathname]);
+
+    checkProductQuantity();
+
+    return (
+      <>
+        {newProductsArray.length > 0 ? (
+          <>
+            <div className={styles.wrapper}>
+              {newProductsArray.map((product) => (
+                <ProductCartCards product={product} key={product.id}></ProductCartCards>
+              ))}
             </div>
-          ))}
-        </div>
-      </div> */}
-    </>
-  );
+            <Footer page={location.pathname} />
+          </>
+        ) : null}
+      </>
+    );
+  }
 }
-
-const url = "https://v2.api.noroff.dev/online-shop";
 
 function ProductCartCards({ product }) {
   const location = useLocation();
@@ -152,4 +150,29 @@ function ProductCartCards({ product }) {
       ) : null}
     </>
   );
+}
+
+{
+  /* <div>
+        {productsInCart.map((product) => (
+          <div key={product.id}>
+            <button onClick={() => dispatch({ type: "addProduct", payload: product })}>Add {product.title}</button>
+            <button onClick={() => dispatch({ type: "removeProduct", payload: product })}>Remove {product.title}</button>
+          </div>
+        ))}
+        <div>
+          <hr />
+          <button onClick={() => dispatch({ type: "clearCart" })}>Clear cart</button>
+          <button onClick={() => dispatch({ type: "getTotal" })}>Get total</button>
+        </div>
+        <div>{state.total}</div>
+        <hr />
+        <div>
+          {state.cart.map((product) => (
+            <div key={product.id}>
+              {product.quantity} - {product.title} - {product.discountedPrice}
+            </div>
+          ))}
+        </div>
+      </div> */
 }
