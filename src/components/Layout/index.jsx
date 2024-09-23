ef2e945ee28a89cce4087a114f3639c8bb0dda07
Thea <theola51924@stud.noroff.no>
2024-09-23 14:18:09 +0700

    added OutletContext with cart State and passed down the cart as a prop through to the Header component

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,20 +1,25 @@
 import { Outlet, useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext } from "react";
 import Header from "./Header/index.jsx";
 import Footer from "./Footer/index.jsx";
 
-export default function Layout() {
+export function Layout() {
+  const [cart, setCart] = useState(["test", "test"]);
   const location = useLocation();
 
-  // useEffect(() => {
-  //   console.log("Location changed");
-  //   console.log(location);
-  // }, [location]);
+  useEffect(() => {
+    let newProductsArray = [];
+    const shoppingCart = JSON.parse(localStorage.getItem("shopping-cart"));
+    newProductsArray = shoppingCart;
+    setCart(newProductsArray);
+    console.log("Cart update on location change - Layout", newProductsArray);
+  }, [location]);
 
+  console.log(cart);
   return (
     <div>
-      <Header />
-      <Outlet />
+      <Header cart={cart} />
+      <Outlet context={{ cart, setCart }} />
       {/* <Footer page={location.pathname} /> */}
     </div>
   );
