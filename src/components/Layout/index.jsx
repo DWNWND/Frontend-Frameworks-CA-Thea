04d9fb2ca8639adcb311982682d0eac73d8ae3d2
Thea <theola51924@stud.noroff.no>
2024-09-23 17:33:14 +0700

    added an initialization of the shopping cart, and passed product, setProduct State and product and setCart as props down to children from Layout component

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,26 +1,37 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useParams } from "react-router-dom";
 import { useState, useEffect, createContext } from "react";
 import Header from "./Header/index.jsx";
 import Footer from "./Footer/index.jsx";
+import useFetch from "../../hooks/useFetch";
 
 export function Layout() {
-  const [cart, setCart] = useState(["test", "test"]);
+  const [cart, setCart] = useState([]);
+  const [product, setProduct] = useState({});
   const location = useLocation();
 
-  useEffect(() => {
+  function initializeCart() {
     let newProductsArray = [];
     const shoppingCart = JSON.parse(localStorage.getItem("shopping-cart"));
-    newProductsArray = shoppingCart;
-    setCart(newProductsArray);
-    console.log("Cart update on location change - Layout", newProductsArray);
-  }, [location]);
+    if (shoppingCart) {
+      if (shoppingCart.length > 0) {
+        newProductsArray = shoppingCart;
+        setCart(newProductsArray);
+        console.log("Cart update on mounting of - Layout", newProductsArray);
+      }
+    } else {
+      console.log("No items in shopping cart");
+    }
+  }
 
-  console.log(cart);
+  useEffect(() => {
+    initializeCart();
+  }, []);
+
   return (
     <div>
       <Header cart={cart} />
-      <Outlet context={{ cart, setCart }} />
-      {/* <Footer page={location.pathname} /> */}
+      <Outlet context={{ product, setProduct, cart, setCart }} />
+      <Footer page={location.pathname} product={product} setCart={setCart} />
     </div>
   );
 }
