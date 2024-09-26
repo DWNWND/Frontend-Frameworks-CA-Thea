0556c18281f7459afeb204cf1383c468e7b04050
Thea <theola51924@stud.noroff.no>
2024-09-26 16:14:49 +0700

    added a function to clear session storage when clicking /home

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -7,11 +7,15 @@ import Cart from "./Cart";
 import styles from "./Header.module.css";
 
 export default function Header({ cart }) {
+  function handleClick() {
+    sessionStorage.removeItem("receipt");
+  }
+  
   return (
     <header className={styles.width}>
       <div className={styles.wrapper}>
         <div className={styles.headerBackground}></div>
-        <Link to="/" className={styles.icon}>
+        <Link to="/" className={styles.icon} onClick={() => handleClick()}>
           <img src={LogoIcon} alt="Lazz logo, click to go to home page" />
         </Link>
         {/* <div className={styled.icon}>
