52604b827b6039811e5ddcfb38d978f663dd5017
Thea <theola51924@stud.noroff.no>
2024-09-30 10:55:37 +0700

    fixed the discount calculation

--- src/calculateDiscount.js
+++ src/calculateDiscount.js
@@ -1,5 +1,7 @@
 export default function calcPercentage(x, y) {
   if (x !== y) {
-    return Math.round((x / y) * 100);
+    const percentage = Math.round((x / y) * 100);
+    const discount = 100 - percentage;
+    return discount;
   }
 }
