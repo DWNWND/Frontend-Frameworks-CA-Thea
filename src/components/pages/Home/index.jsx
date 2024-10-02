1bc99c61d62dce019a6ab46d90f170bb930abe3b
Thea <theola51924@stud.noroff.no>
2024-10-02 11:34:36 +0700

    added a Helmet to the Home component

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -2,13 +2,20 @@ import { ProductsToDisplay } from "../../ProductCards";
 import Categories from "../../Categories";
 import BannerCommercial from "../../BannerCommercial";
 import styles from "./Home.module.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export default function Home() {
   return (
-    <div className={styles.wrapper}>
-      <BannerCommercial />
-      <Categories />
-      <ProductsToDisplay />
-    </div>
+    <HelmetProvider>
+      <Helmet prioritizeSeoTags>
+        <meta name="description" content="" />
+        <title>Lazz | Shop Flash Sale!</title>
+      </Helmet>
+      <div className={styles.wrapper}>
+        <BannerCommercial />
+        <Categories />
+        <ProductsToDisplay />
+      </div>
+    </HelmetProvider>
   );
 }
