c6205b8aa3a8fd84f9e8eb393d2847f36df57687
Thea <theola51924@stud.noroff.no>
2024-10-04 12:13:13 +0700

    removed the formState: isSubmitSUccessful as its native

--- src/components/pages/Contact/index.jsx
+++ src/components/pages/Contact/index.jsx
@@ -43,7 +43,6 @@ function ContactForm() {
     handleSubmit,
     reset,
     formState,
-    formState: { isSubmitSuccessful },
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
