40c326d3335c8633035a151e7eeefda8bd4cddc0
Thea <theola51924@stud.noroff.no>
2024-10-06 13:50:25 +0700

    recreated the Router according to this source: https://reactrouter.com/en/main/upgrading/v6-data, the scrollToTop does not work now

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,30 @@ import Checkout from "./components/pages/Checkout";
 import CheckoutSuccess from "./components/pages/CheckoutSuccess";
 import Contact from "./components/pages/Contact";
 import ScrollToTop from "./scrollToTop";
-import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([{ path: "*", element: <Root /> }]);
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function RouteNotFound() {
   return <div>Page not found</div>;
 }
 
+//recreated router according to this source: https://reactrouter.com/en/main/upgrading/v6-data
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "products/:category", element: <ProductsList /> },
+      { path: "product/:id", element: <Product /> },
+      { path: "checkout", element: <Checkout /> },
+      { path: "success", element: <CheckoutSuccess /> },
+      { path: "contact", element: <Contact /> },
+      { path: "*", element: <RouteNotFound /> },
+    ],
+  },
+]);
+
 export default function App() {
   return <RouterProvider router={router} />;
-}
-
-function Root() {
-  return (
-    <>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="products/:category" element={<ProductsList />} />
-          <Route path="product/:id" element={<Product />} />
-          <Route path="checkout" element={<Checkout />} />
-          <Route path="success" element={<CheckoutSuccess />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<RouteNotFound />} />
-        </Route>
-      </Routes>
-    </>
-  );
 }
