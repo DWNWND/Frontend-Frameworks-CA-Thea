e78ded520b699c37f175dca6b127cc7bcc9bccc7
Thea <theola51924@stud.noroff.no>
2024-10-06 13:27:59 +0700

    first step in changing from BrowserRouter to RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,19 @@ import App from "./App.jsx";
 import "./index.css";
 import Theme from "./styles/theme.jsx";
 import GlobalStyle from "./styles/globalStyle.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([{ path: "*", element: <App /> }]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Theme>
-      <BrowserRouter>
-        <GlobalStyle />
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
+      {/* <BrowserRouter> */}
+      <GlobalStyle />
+      {/* <App /> */}
+      {/* </BrowserRouter> */}
+      {/* </RouterProvider> */}
     </Theme>
   </StrictMode>
 );
