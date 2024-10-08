409bed98858829d002b97fee565bd55fd68da877
Thea <theola51924@stud.noroff.no>
2024-10-08 07:47:24 +0700

    updated rating function/component with shorter quicker syntax!

--- src/components/Ratings/index.jsx
+++ src/components/Ratings/index.jsx
@@ -1,5 +1,8 @@
 import styles from "./Ratings.module.css";
 
+const star = "★";
+const noStar = "☆";
+
 function ReviewLength({ section, reviews }) {
   if (section === "reviews") {
     return <></>;
@@ -9,52 +12,19 @@ function ReviewLength({ section, reviews }) {
 }
 
 export default function Ratings({ rating, reviews, section }) {
-  if (rating < 1) {
-    return (
-      <div className={styles.ratings}>
-        <p className={styles.star}>☆ ☆ ☆ ☆ ☆</p>
-        <ReviewLength section={section} reviews={reviews} />
-      </div>
-    );
+  let stars = "";
+
+  for (let i = 0; i < 5; i++) {
+    if (i < rating) {
+      stars += star;
+    } else {
+      stars += noStar;
+    }
   }
-  if (rating >= 1 && rating < 2) {
-    return (
-      <div className={styles.ratings}>
-        <p className={styles.star}>★ ☆ ☆ ☆ ☆</p>
-        <ReviewLength section={section} reviews={reviews} />
-      </div>
-    );
-  }
-  if (rating >= 2 && rating < 3) {
-    return (
-      <div className={styles.ratings}>
-        <p className={styles.star}>★ ★ ☆ ☆ ☆</p>
-        <ReviewLength section={section} reviews={reviews} />
-      </div>
-    );
-  }
-  if (rating >= 3 && rating < 4) {
-    return (
-      <div className={styles.ratings}>
-        <p className={styles.star}>★ ★ ★ ☆ ☆</p>
-        <ReviewLength section={section} reviews={reviews} />
-      </div>
-    );
-  }
-  if (rating >= 4 && rating < 5) {
-    return (
-      <div className={styles.ratings}>
-        <p className={styles.star}>★ ★ ★ ★ ☆</p>
-        <ReviewLength section={section} reviews={reviews} />
-      </div>
-    );
-  }
-  if (rating >= 5) {
-    return (
-      <div className={styles.ratings}>
-        <p className={styles.star}>★ ★ ★ ★ ★</p>
-        <ReviewLength section={section} reviews={reviews} />
-      </div>
-    );
-  }
+  return (
+    <div className={styles.ratings}>
+      <p className={styles.star}>{stars ? stars : null}</p>
+      <ReviewLength section={section} reviews={reviews} />
+    </div>
+  );
 }
