d6d95a406353d711db00817bdfb1323a5c625651
Thea <theola51924@stud.noroff.no>
2024-10-10 15:32:35 +0700

    moved the ProductCards component to its own folder inside the Products folder

--- src/components/ProductCards/ProductLists/index.jsx
+++ src/components/ProductCards/ProductLists/index.jsx
@@ -1,8 +1,8 @@
 import { Link, useLocation, useOutletContext, useParams } from "react-router-dom";
 import styles from "./ProductCards.module.css";
-import Price from "../Price/index.jsx";
-import Ratings from "../Ratings/index.jsx";
-import Filters from "../Filters/index.jsx";
+import Price from "../../Price/index.jsx";
+import Ratings from "../../Ratings/index.jsx";
+import Filters from "../../Filters/index.jsx";
 
 export default function ProductsToDisplay({ tag }) {
   const { products, isLoading, isError } = useOutletContext();
